d6b0e59704e42e2308d6dd296e416dfa2ec2112e
fathurrobb <ilhamfathurrobbani@gmail.com>
2022-11-13 00:11:36 +0700

    register done

--- src/Components/ButtonAction.jsx
+++ src/Components/ButtonAction.jsx
@@ -1,8 +1,8 @@
 import { Button } from "react-bootstrap";
 
-const ButtonAction = ({variant,text,onClick,className,children}) => {
+const ButtonAction = ({variant,text,onClick,className,disabled,children}) => {
     return (
-        <Button variant={variant} className={className} onClick={onClick} type="submit">{children}{text}</Button>
+        <Button variant={variant} className={className} onClick={onClick} disabled={disabled} type="submit">{children}{text}</Button>
     )   
 }
 
