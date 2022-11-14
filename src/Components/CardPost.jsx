d2688f60e2977f2c97e3f650ac76514d44517338
khairnsaa <k.nsaa95@gmail.com>
2022-11-14 19:53:38 +0700

    fix sidebar logout

--- src/Components/CardPost.jsx
+++ src/Components/CardPost.jsx
@@ -1,4 +1,4 @@
-import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faComment, faHeart, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faRetweet } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card } from "react-bootstrap";
@@ -22,6 +22,7 @@ const CardPost = (post) => {
                 <FontAwesomeIcon className="icon" icon={faComment} />
                 <FontAwesomeIcon className="icon" icon={faRetweet} />
                 <FontAwesomeIcon className="icon" icon={faHeart} />
+                <FontAwesomeIcon className="icon" icon={faTrashCan} />
             </div>
 
             {/* <Card style={{ cursor: 'pointer' }} border="light" className='my-2' bg='dark' key='dark' text='white' onClick={() => toDetailPost(post.post.id)}>
