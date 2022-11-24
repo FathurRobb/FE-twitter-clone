c1d3654bc48b69cceecb9e03d978752e379dd739
Ristan Alfatha Aditara <ristanaa09@gmail.com>
2022-11-24 20:04:45 +0800

    tes rebase

--- src/Components/AlertAction.jsx
+++ src/Components/AlertAction.jsx
@@ -1,6 +1,7 @@
 import { Alert } from "react-bootstrap";
 
 const AlertAction = ({variant,message,onClose}) => {
+    // tesss
     return (
         <Alert variant={variant} onClose={onClose} dismissible>{message}</Alert>
     )   
